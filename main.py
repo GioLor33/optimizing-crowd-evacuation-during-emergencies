a4d7a5fcab5bdd64c403b8e1b5c782bc1d57ec67
Giorgia Lorengo <giorgia.lorengo@gmail.com>
2025-11-13 15:03:11 +0100

    Assert added

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import asyncio
 from visualization.visualizer import Visualizer
 
 async def visualization_loop(visualizer):
+    assert isinstance(visualizer, Visualizer)
     while visualizer.window_is_open() and visualizer.on:
         visualizer.create_drawing()
         await asyncio.sleep(0)  # yield control
 
 async def main_program(visualizer):
+    assert isinstance(visualizer, Visualizer)
     loop = asyncio.get_running_loop()
     while True:
         # Main executions
         pass
         
 
-async def main():
+async def initialize_main():
     visualizer = Visualizer(name="Crowd Simulation")
     await asyncio.gather(
         visualization_loop(visualizer),
@@ -21,4 +23,4 @@ async def main():
     )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(initialize_main())
